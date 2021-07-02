8c6b549d28018d01433e344aec30c9454b5d1274
veplotnikov <80313261+veplotnikov@users.noreply.github.com>
2021-07-02 15:23:49 +0700

    Create config.tf

---
+++ config.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.74.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+
+  resource "google_service_account" "default" {
+  account_id   = "114438999861948486183"
+  display_name = "786418527307-compute"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "europe-west2-c"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-focal-v20210623"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  # metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = 786418527307-compute@developer.gserviceaccount.coml
+    scopes = ["cloud-platform"]
+  }
+}
+}
