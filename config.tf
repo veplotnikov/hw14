cbcd13e4b1f95b0a0b1fc79927e39def5dd8ed21
veplotnikov <80313261+veplotnikov@users.noreply.github.com>
2021-07-03 01:22:12 +0700

    Update config.tf

--- config.tf
+++ config.tf
@@ -43,7 +43,8 @@ resource "google_compute_instance" "default" {
   }
 
   metadata_startup_script = "${file("startup.sh")}"
-
+}
+ 
   output "ip" {
   value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
      }
@@ -61,4 +62,4 @@ resource "google_compute_instance" "default" {
       }
   }
 
-}
+
