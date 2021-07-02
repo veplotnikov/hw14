3ac015e23b4d2c350c4302f691d5968631edd949
veplotnikov <80313261+veplotnikov@users.noreply.github.com>
2021-07-03 05:59:49 +0700

    Update startup_prod.sh

--- startup_prod.sh
+++ startup_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo gcloud auth activate-service-account hw14-86@principal-rope-318517.iam.gserviceaccount.com --key-file=/home/test/credentials.json --project=principal-rope-318517
 sudo apt-get update && sudo apt-get install tomcat9 -y
-gsutil rsync -r  gs://hw14-data/ /opt/
+sudo gcloud auth activate-service-account hw14-86@principal-rope-318517.iam.gserviceaccount.com --key-file=/home/test/credentials.json --project=principal-rope-318517
+gsutil rsync -r  gs://hw14-data/ /var/lib/tomcat9/webapps/
