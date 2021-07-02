5291ad5a6fa397f4b9dd3c71700b2886fb75af4f
veplotnikov <80313261+veplotnikov@users.noreply.github.com>
2021-07-03 05:02:38 +0700

    Update config.tf

--- config.tf
+++ config.tf
@@ -17,7 +17,7 @@ zone   = "europe-west2-c"
 }
 
 resource "google_compute_instance" "default" {
-  name         = "test"
+  name         = "build"
   machine_type = "e2-medium"
   zone         = "europe-west2-c"
 
@@ -57,6 +57,7 @@ resource "google_compute_instance" "default" {
       }
   }
 
+
 metadata_startup_script = "${file("startup.sh")}"
 
 
@@ -65,6 +66,62 @@ metadata_startup_script = "${file("startup.sh")}"
   output "ip" {
   value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
      }
+
+provider "google" {
+  # Configuration options
+
+credentials = "${file("/opt/credentials.json")}"
+project = "principal-rope-318517"
+region = "europe-west2"
+zone   = "europe-west2-c"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "prod"
+  machine_type = "e2-medium"
+  zone         = "europe-west2-c"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-focal-v20210623"
+    }
+  }
+
+  
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+ metadata = {
+    ssh-keys = "test:${file("/my_test_key.pub")}"
+  }
+
+ 
+
+ provisioner "file" {
+  source = "/opt/credentials.json"
+  destination = "/home/test/credentials.json"
+
+  connection {
+    type = "ssh"
+    user = "test"
+    private_key = "${file("/my_test_key")}"
+    agent = "false"
+    host = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+      }
+  }
+
+
+metadata_startup_script = "${file("startup_prod.sh")}"
+
+
+}
 
 
 
