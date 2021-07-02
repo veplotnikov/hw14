d194b694d15a7a112242cb5df2743e764f0db402
veplotnikov <80313261+veplotnikov@users.noreply.github.com>
2021-07-03 03:30:15 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,7 +8,7 @@ sudo apt-get update && apt-get install apt-transport-https ca-certificates gnupg
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk -y
 # Auth
-gcloud auth activate-service-account hw14-86@principal-rope-318517.iam.gserviceaccount.com --key-file=/home/test/credentials.json --project=principal-rope-318517
+#gcloud auth activate-service-account hw14-86@principal-rope-318517.iam.gserviceaccount.com --key-file=/home/test/credentials.json --project=principal-rope-318517
 # Build app
 mkdir /opt/boxfuse 
 mkdir -p /opt/tomcat/latest/webapps/
@@ -16,4 +16,4 @@ git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git /opt/boxf
 mvn -f /opt/boxfuse/pom.xml package
 cp /opt/boxfuse/target/hello-1.0.war /opt/tomcat/latest/webapps/
 # Copy apps to basket
-gsutil rsync -r /opt/tomcat/latest/webapps/ gs://hw14-data/
+#gsutil rsync -r /opt/tomcat/latest/webapps/ gs://hw14-data/
