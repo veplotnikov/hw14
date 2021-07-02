124063cc1dc4340e5693bf7bb84e42c70e3f21b1
veplotnikov <80313261+veplotnikov@users.noreply.github.com>
2021-07-02 21:18:46 +0700

    Update config.tf

--- config.tf
+++ config.tf
@@ -47,9 +47,4 @@ resource "google_compute_instance" "default" {
 
   # metadata_startup_script = "echo hi > /test.txt"
 
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = 786418527307-compute
-    scopes = ["cloud-platform"]
-  }
 }
